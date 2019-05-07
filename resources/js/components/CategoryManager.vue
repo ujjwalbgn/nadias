9042864b57c42d3fef410305469ce1cd17df0d8c
Ujjwal Bajagain <ubajagain@gmail.com>
2019-05-07 14:55:27 -0500

    scrolling and focus added

--- resources/js/components/CategoryManager.vue
+++ resources/js/components/CategoryManager.vue
@@ -1,12 +1,14 @@
 <template>
     <form>
+        <a @click="addCategory" class="add">+ Add Category</a>
         <div v-for="(category,index) in categories" :key="category.id">
-            <input type="text" v-model="category.name">
+            <input type="text" v-model="category.name" :ref="category.name">
             <input type="text" v-model="category.display_order">
             <a @click="removeCategory(index)" class="remove">Delete</a>
             <div>
-                <img :src="`/images/${category.image}`" width="100">
-                <img type="text" v-model="category.image">
+                <img v-if="category.image" :src="`/images/${category.image}`" width="100">
+                <level v-else>Image</level>
+                <input type="text" v-model.lazy="category.image">
             </div>
             <hr>
         </div>
@@ -29,6 +31,19 @@
                 if(confirm("Are you sure ?")){
                     this.categories.splice(index,1);
                 }
+            },
+
+            addCategory(){
+                this.categories.push({
+                    id: 0,
+                    name: '',
+                    image: '',
+                    display_order: this.categories.length + 1
+                });
+                this.$nextTick(()=>{
+                    window.scrollTo(0, document.body.scrollHeight);
+                    this.$refs[''][0].focus();
+                });
             }
         }
     }
